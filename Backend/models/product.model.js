0380571f5085aa3898a9536f0a020b34b31e9cc1
Your Name <edickson113@rku.ac.in.com>
2026-01-17 22:12:46 +0530

    initial schema of models

---
+++ Backend/models/product.model.js
@@ -0,0 +1,49 @@
+import mongoose, { mongo } from "mongoose";
+
+const ProductSchema = mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  desc: {
+    type: String,
+    require: true,
+  },
+  img: {
+    type: String,
+    require: true,
+  },
+  video: {
+    type: String,
+  },
+  categories: {
+    type: Array,
+  },
+  concern: {
+    type: Array,
+  },
+  brand: {
+    type: String,
+  },
+  originalPrice: {
+    type: Number,
+  },
+  discountedPrice: {
+    type: Number,
+  },
+  inStock: {
+    type: Boolean,
+    default: true,
+  },
+  ratings: [
+    {
+      star: { type: String },
+      name: { type: string },
+      comment: { type: String },
+      postedBy: { type: String },
+    },
+  ],
+});
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
