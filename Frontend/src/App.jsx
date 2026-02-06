1b66bd2f9bd47086255b14f9e5be43a23efffa56
Your Name <edickson113@rku.ac.in.com>
2026-02-06 15:27:42 +0530

    Begin frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Myaccount from "./pages/Myaccount";
+
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Announcement from "./components/Announcement";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Announcement />
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+
+        {
+          path: "/myaccount",
+          element: <Myaccount />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
